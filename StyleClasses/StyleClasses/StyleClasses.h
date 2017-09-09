5b3a5066f3cc31f80ce3063f9ea65685cec6fc21
Aleksey Zgurskiy <mr.noone@icloud.com>
2017-09-09 15:17:12 +0300

    Initial commit

---
+++ StyleClasses/StyleClasses/StyleClasses.h
@@ -0,0 +1,12 @@
+//
+//  StyleClasses.h
+//  StyleClasses
+//
+//  Created by Aleksey Zgurskiy on 09.09.17.
+//  Copyright © 2017 Aleksey Zgurskiy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+FOUNDATION_EXPORT double StyleClassesVersionNumber;
+FOUNDATION_EXPORT const unsigned char StyleClassesVersionString[];
